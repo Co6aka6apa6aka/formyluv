e7f4f4fdb14dc627bf65bebbe3d76378919bae08
Co6aka6apa6aka <denistabunsik@gmail.com>
2024-10-31 16:40:33 +0200

    Первый коммит: добавление проекта

---
+++ FrMyLuv/Pages/Words.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrMyLuv.Pages
+{
+    public class WordsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
